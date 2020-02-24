055c66c568e39a7a277ad535728d881e0e8ca09e
Steve Grossi <me@stevegrossi.com>
2020-02-24 13:10:23 -0500

    Adds “Done” screen and avoids duplicates

--- lib/name_game_web/live/flash_cards_live.ex
+++ lib/name_game_web/live/flash_cards_live.ex
@@ -2,50 +2,65 @@ defmodule NameGameWeb.FlashCardsLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
-    data =
-      ConCache.get_or_store(:data_cache, :data, fn() ->
-        fetch_data()
-      end)
-
+    data = load_data()
     socket = assign(socket, :people, data)
     socket = assign(socket, :count, 1)
-    socket = assign(socket, :current, List.first(socket.assigns.people).name)
+    socket = assign(socket, :done, false)
+    socket = assign(socket, :current_id, List.first(data).photo)
     socket = assign(socket, :revealed, false)
+    socket = assign(socket, :total, Enum.count(data))
+
     {:ok, socket}
   end
 
   def render(assigns) do
-    current =
+    current_person =
       Enum.find(assigns.people, fn(person) ->
-        person.name == assigns.current
+        person.photo == assigns.current_id
       end)
 
     ~L"""
     <div class="stage">
       <div class="count">
-        <%= @count %> / <%= Enum.count(@people) %>
+        <%= @count %> / <%= @total %>
       </div>
-      <img class="photo" src="<%= current.photo %>" alt="" />
-      <div class="placeholder <%= if not @revealed, do: "revealed" %>">?</div>
-      <div class="namecard <%= if @revealed, do: "revealed" %>">
-        <h2><%= current.name %></h2>
-        <p><%= current.title %></p>
-      </div>
-    </div>
-    <p class="buttons">
-      <%= if @revealed do %>
-        <button class="button" phx-click="shuffle">Another!</button>
+      <%= if @done do %>
+        <div class="done">
+          <h2>That’s everyone!</h2>
+          <p>Feeling ambitious? Refresh the page to start over!</p>
+        </div>
       <% else %>
-        <button class="button" phx-click="reveal">Show me!</button>
+        <img class="photo" src="<%= current_person.photo %>" alt="" />
+        <div class="placeholder <%= if not @revealed, do: "revealed" %>">?</div>
+        <div class="namecard <%= if @revealed, do: "revealed" %>">
+          <h2><%= current_person.name %></h2>
+          <p><%= current_person.title %></p>
+        </div>
       <% end %>
-    </p>
+    </div>
+    <%= if not @done do %>
+      <p class="buttons">
+        <%= if @revealed do %>
+          <button class="button" phx-click="next">Another!</button>
+        <% else %>
+          <button class="button" phx-click="reveal">Show me!</button>
+        <% end %>
+      </p>
+    <% end %>
     """
   end
 
-  def handle_event("shuffle", _event, socket) do
+  def handle_event("next", _event, %{assigns: %{count: _same, total: _same}} = socket) do
+    socket = assign(socket, :done, true)
+    {:noreply, socket}
+  end
+  def handle_event("next", _event, socket) do
     socket = update(socket, :count, &(&1 + 1))
     socket = assign(socket, :revealed, false)
-    socket = assign(socket, :current, Enum.random(socket.assigns.people).name)
+    socket = assign(socket, :people, Enum.reject(socket.assigns.people, fn(person) ->
+      person.photo == socket.assigns.current_id
+    end))
+    socket = assign(socket, :current_id, Enum.random(socket.assigns.people).photo)
     {:noreply, socket}
   end
 
@@ -54,20 +69,22 @@ defmodule NameGameWeb.FlashCardsLive do
     {:noreply, socket}
   end
 
-  defp fetch_data() do
-    HTTPoison.start
-    response = HTTPoison.get!("https://www.lessonly.com/team/")
-    {:ok, document} = Floki.parse_document(response.body)
-    members = Floki.find(document, ".team-member")
-    Enum.map(members, fn(member_node) ->
-      [photo] = Floki.attribute(member_node, ".team-member-photo .photo-hover", "data-src")
-      name = Floki.find(member_node, ".team-member-name") |> Floki.text
-      title = Floki.find(member_node, ".team-member-title") |> Floki.text
-      %{
-        name: name,
-        title: title,
-        photo: photo
-      }
+  defp load_data() do
+    ConCache.get_or_store(:data_cache, :data, fn() ->
+      HTTPoison.start
+      response = HTTPoison.get!("https://www.lessonly.com/team/")
+      {:ok, document} = Floki.parse_document(response.body)
+      members = Floki.find(document, ".team-member")
+      Enum.map(members, fn(member_node) ->
+        [photo] = Floki.attribute(member_node, ".team-member-photo .photo-hover", "data-src")
+        name = Floki.find(member_node, ".team-member-name") |> Floki.text
+        title = Floki.find(member_node, ".team-member-title") |> Floki.text
+        %{
+          name: name,
+          title: title,
+          photo: photo
+        }
+      end)
     end)
   end
 end
