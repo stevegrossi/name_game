9593c3f38fd6ad51aaa771e947b325a8ba21ed73
Steve Grossi <me@stevegrossi.com>
2020-02-24 10:29:50 -0500

    Set up custom domain on Gigalixir

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,7 @@ import Config
 config :name_game, NameGameWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: "namegame.stevegrossi.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["//namegame.stevegrossi.com"],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
